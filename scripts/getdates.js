76c206c7be0325db5ef7bcb0a6ac582d58c8eef2
ajuanillo <ajuanillo@byupathway.edu>
2025-01-23 00:32:08 -0500

    changes

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -7,36 +7,45 @@ const year = document.querySelector("#year");
 const month = document.querySelector("#month");
 const day = document.querySelector("#day");
 const dayofweek = document.querySelector("#dayofweek");
+const lastmod = document.querySelector("#lastmodified");
 
 // use the date object
 const today = new Date();
+if (full!=null)
+{
+	full.innerHTML = `Today is <span class="highlight">${new Intl.DateTimeFormat(
+		"en-US",
+		{
+			dateStyle: "full"
+		}
+	).format(today)}</span>`;
+}
 
-full.innerHTML = `Today is <span class="highlight">${new Intl.DateTimeFormat(
-	"en-US",
-	{
-		dateStyle: "full"
-	}
-).format(today)}</span>`;
+/*
 worldfull.innerHTML = `UK: <span class="highlight">${new Intl.DateTimeFormat(
 	"en-UK",
 	{
 		dateStyle: "full"
 	}
-).format(today)}</span>`;
-short.innerHTML = `Short: <span class="highlight">${new Intl.DateTimeFormat(
-	"en-US",
+).format(today)}</span>`; */
+if (short!=null)
 	{
-		dateStyle: "short"
+	short.innerHTML = `<span class="highlight">${new Intl.DateTimeFormat(
+		"en-US",
+		{
+			dateStyle: "short"
+		}
+	).format(today)}</span>`; 
 	}
-).format(today)}</span>`;
-medium.innerHTML = `Medium: <span class="highlight">${new Intl.DateTimeFormat(
+/* medium.innerHTML = `Medium: <span class="highlight">${new Intl.DateTimeFormat(
 	"en-US",
 	{
 		dateStyle: "medium"
 	}
-).format(today)}</span>`;
+).format(today)}</span>`;  */
 
-year.innerHTML = `getFullYear(): <span class="highlight">${today.getFullYear()}</span>`;
-month.innerHTML = `getMonth(): <span class="highlight">${today.getMonth()}</span>`;
-day.innerHTML = `getDate(): <span class="highlight">${today.getDate()}</span>`;
-dayofweek.innerHTML = `getDay(): <span class="highlight">${today.getDay()}</span>`;
+year.innerHTML = ` <span class="highlight">${today.getFullYear()}</span>`;
+//month.innerHTML = `getMonth(): <span class="highlight">${today.getMonth()}</span>`;
+//day.innerHTML = `getDate(): <span class="highlight">${today.getDate()}</span>`;
+// dayofweek.innerHTML = `getDay(): <span class="highlight">${today.getDay()}</span>`;
+lastmod.innerHTML = `Last modification <span class="highlight">${document.lastModified}</span>`;
