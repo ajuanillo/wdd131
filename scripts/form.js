63a4a700660f3d8ad321c5bf74e0c06d99d1e434
ajuanillo <ajuanillo@byupathway.edu>
2025-02-20 13:56:34 -0500

    update form info

--- scripts/form.js
+++ scripts/form.js
@@ -1,8 +1,30 @@
 const products = [
-  { id: 1, name: "Widget" },
-  { id: 2, name: "Gadget" },
-  { id: 3, name: "Tool" }
-];
+    {
+      id: "fc-1888",
+      name: "flux capacitor",
+      averagerating: 4.5
+    },
+    {
+      id: "fc-2050",
+      name: "power laces",
+      averagerating: 4.7
+    },
+    {
+      id: "fs-1987",
+      name: "time circuits",
+      averagerating: 3.5
+    },
+    {
+      id: "ac-2000",
+      name: "low voltage reactor",
+      averagerating: 3.9
+    },
+    {
+      id: "jj-1969",
+      name: "warp equalizer",
+      averagerating: 5.0
+    }
+  ];
 
 const productSelect = document.getElementById("product");
 products.forEach((product) => {
